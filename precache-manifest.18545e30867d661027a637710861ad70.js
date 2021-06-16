b43599fb0ce3e7db8b7993e368a7e26a17a5463c
KRAK94 <andrekravchenko1994@mail.ru>
2021-06-16 22:30:49 +0300

    Updates

---
+++ precache-manifest.18545e30867d661027a637710861ad70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b559d9c18c1c36f0eb8987c808878a3",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "51ff8617dc4b7f1a58dd",
+    "url": "/todolist/static/css/main.ef8b6906.chunk.css"
+  },
+  {
+    "revision": "dc39d836c561276e5fa4",
+    "url": "/todolist/static/js/2.cd4b9d07.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/todolist/static/js/2.cd4b9d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ff8617dc4b7f1a58dd",
+    "url": "/todolist/static/js/main.75a40bf9.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
